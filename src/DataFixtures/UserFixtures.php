908a83899cf206d37c7408e73a8ad048c53e6f30
johannes60-sk <garilsenan@gmail.com>
2023-10-04 14:14:51 +0200

    ajout de l'authentification

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    private $passwordHasher;
+
+    public function __construct( UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+        
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+      
+        $user = new User();
+
+        $user->setUsername('demo');
+
+        $hashedPassword = $this->passwordHasher->hashPassword($user, 'demo');
+
+        $user->setPassword($hashedPassword);
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
